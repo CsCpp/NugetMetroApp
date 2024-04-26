22186d2ed58b85ac41a9a2b921e2c22b870e013e
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-04-26 14:54:45 +0300

    Скрыть элементы

--- NugetMetroApp/Form1.Designer.cs
+++ NugetMetroApp/Form1.Designer.cs
@@ -34,16 +34,22 @@
             this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
             this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
             this.metroRadioButton1 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroTrackBar1 = new MetroFramework.Controls.MetroTrackBar();
+            this.metroToolTip1 = new MetroFramework.Components.MetroToolTip();
+            this.metroScrollBar1 = new MetroFramework.Controls.MetroScrollBar();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
             this.SuspendLayout();
             // 
             // metroStyleManager1
             // 
             this.metroStyleManager1.Owner = null;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
             // 
             // metroButton1
             // 
-            this.metroButton1.Location = new System.Drawing.Point(65, 112);
+            this.metroButton1.Location = new System.Drawing.Point(23, 38);
             this.metroButton1.Name = "metroButton1";
             this.metroButton1.Size = new System.Drawing.Size(173, 52);
             this.metroButton1.TabIndex = 0;
@@ -53,25 +59,26 @@
             // 
             this.metroComboBox1.FormattingEnabled = true;
             this.metroComboBox1.ItemHeight = 24;
-            this.metroComboBox1.Location = new System.Drawing.Point(529, 228);
+            this.metroComboBox1.Location = new System.Drawing.Point(23, 115);
             this.metroComboBox1.Name = "metroComboBox1";
-            this.metroComboBox1.Size = new System.Drawing.Size(121, 30);
+            this.metroComboBox1.Size = new System.Drawing.Size(173, 30);
             this.metroComboBox1.TabIndex = 1;
             // 
             // metroCheckBox1
             // 
             this.metroCheckBox1.AutoSize = true;
-            this.metroCheckBox1.Location = new System.Drawing.Point(528, 112);
+            this.metroCheckBox1.Location = new System.Drawing.Point(271, 38);
             this.metroCheckBox1.Name = "metroCheckBox1";
-            this.metroCheckBox1.Size = new System.Drawing.Size(121, 17);
+            this.metroCheckBox1.Size = new System.Drawing.Size(128, 17);
             this.metroCheckBox1.TabIndex = 2;
-            this.metroCheckBox1.Text = "metroCheckBox1";
+            this.metroCheckBox1.Text = "Скрыть элементы";
             this.metroCheckBox1.UseVisualStyleBackColor = true;
+            this.metroCheckBox1.CheckedChanged += new System.EventHandler(this.metroCheckBox1_CheckedChanged);
             // 
             // metroRadioButton1
             // 
             this.metroRadioButton1.AutoSize = true;
-            this.metroRadioButton1.Location = new System.Drawing.Point(529, 289);
+            this.metroRadioButton1.Location = new System.Drawing.Point(271, 73);
             this.metroRadioButton1.Name = "metroRadioButton1";
             this.metroRadioButton1.Size = new System.Drawing.Size(137, 17);
             this.metroRadioButton1.TabIndex = 3;
@@ -79,17 +86,47 @@
             this.metroRadioButton1.Text = "metroRadioButton1";
             this.metroRadioButton1.UseVisualStyleBackColor = true;
             // 
+            // metroTrackBar1
+            // 
+            this.metroTrackBar1.BackColor = System.Drawing.Color.Transparent;
+            this.metroTrackBar1.Location = new System.Drawing.Point(23, 170);
+            this.metroTrackBar1.Name = "metroTrackBar1";
+            this.metroTrackBar1.Size = new System.Drawing.Size(385, 23);
+            this.metroTrackBar1.TabIndex = 4;
+            this.metroTrackBar1.Text = "metroTrackBar1";
+            this.metroTrackBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.metroTrackBar1_Scroll);
+            // 
+            // metroScrollBar1
+            // 
+            this.metroScrollBar1.Location = new System.Drawing.Point(437, 38);
+            this.metroScrollBar1.Name = "metroScrollBar1";
+            this.metroScrollBar1.Orientation = MetroFramework.Controls.MetroScrollOrientation.Vertical;
+            this.metroScrollBar1.Size = new System.Drawing.Size(10, 155);
+            this.metroScrollBar1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 43);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(469, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.metroScrollBar1);
+            this.Controls.Add(this.metroTrackBar1);
             this.Controls.Add(this.metroRadioButton1);
             this.Controls.Add(this.metroCheckBox1);
             this.Controls.Add(this.metroComboBox1);
             this.Controls.Add(this.metroButton1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -103,6 +140,10 @@
         private MetroFramework.Controls.MetroComboBox metroComboBox1;
         private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
         private MetroFramework.Controls.MetroRadioButton metroRadioButton1;
+        private MetroFramework.Controls.MetroTrackBar metroTrackBar1;
+        private MetroFramework.Components.MetroToolTip metroToolTip1;
+        private MetroFramework.Controls.MetroScrollBar metroScrollBar1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- NugetMetroApp/Form1.cs
+++ NugetMetroApp/Form1.cs
@@ -17,5 +17,32 @@ namespace NugetMetroApp
             InitializeComponent();
             this.StyleManager = metroStyleManager1;
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(metroCheckBox1.Checked) 
+            {
+            metroComboBox1.Visible = false;
+            metroScrollBar1.Visible = false;
+                metroButton1.Visible = false;
+             
+            }
+            else
+            {
+                metroComboBox1.Visible = true;
+                metroScrollBar1.Visible = true;
+                metroButton1.Visible = true;
+            }
+        }
+
+        private void metroTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
     }
 }
